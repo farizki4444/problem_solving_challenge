660bbdc165e0ffd7b70bd611f10a2f948d267510
farizki4444 <hermawanfarizki4444@gmail.com>
2021-10-14 19:18:14 +0700

    problem 7

---
+++ Assets/script/CubeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public GameObject cube;
+    public float xMin, xMax, yMin, yMax;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+    void Spawn()
+    {
+        int countObject = Random.Range(4, 8);
+        for (int i = 0; i < countObject; i++)
+        {
+            float randomX = Random.Range(xMin, xMax);
+            float randomY = Random.Range(yMin, yMax);
+            Instantiate(cube, new Vector2(randomX, randomY), Quaternion.identity);
+        }
+    }
+
+   
+}
+
---
+++ Assets/script/UIScore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScore : MonoBehaviour
+{
+    public Text Score;
+    public ScoreManager ScoreManager;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        Score.text ="Score: " + ScoreManager.GetCurrentScore().ToString();
+        
+    }
+}
