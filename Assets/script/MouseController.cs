927b5233551345362c5eb04cef80b67b1d72eae1
farizki4444 <hermawanfarizki4444@gmail.com>
2021-10-14 20:13:13 +0700

    problem8

---
+++ Assets/script/MouseController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    private Rigidbody2D BallRigidBody;
+    public Camera MainCamera;
+
+    [Header("Ball Movement")]
+    public Vector2 ballAngle = new Vector2(100, 100);
+    public float speed;
+    public ScoreManager ScoreManager;
+
+    private void Start()
+    {
+        BallRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector3 target = MainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+        Vector3 moveAngle = target - transform.position;
+
+        if (Vector3.Distance(target, transform.position) > 0.5)
+            transform.Translate(moveAngle.normalized * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Qube")
+
+
+        {
+            ScoreManager.IncrementScore();
+            Destroy(col.gameObject);
+        }
+
+
+
+    }
+}
